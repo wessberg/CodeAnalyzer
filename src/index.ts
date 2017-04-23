9821aac915a710d0f185045f87879af622b28462
wessberg <frederikwessberg@gmail.com>
2017-04-24 01:02:53 +0200

    Added an index file for all exports

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export {BindingIdentifier} from "src/BindingIdentifier";
+export {SimpleLanguageService} from "src/SimpleLanguageService";
+export {ISimpleLanguageService, IClassDeclaration, IModuleDependency, InitializationValue, NullableInitializationValue} from "./interface/ISimpleLanguageService";
+export {ISimpleLanguageServiceConfig} from "./interface/ISimpleLanguageServiceConfig";
