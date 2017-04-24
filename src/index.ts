4e4817a48c277a5502b5129339666ab231e03cde
wessberg <frederikwessberg@gmail.com>
2017-04-24 02:33:06 +0200

    More exported interfaces and types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export {BindingIdentifier} from "./BindingIdentifier";
 export {SimpleLanguageService} from "./SimpleLanguageService";
-export {ISimpleLanguageService, IClassDeclaration, IModuleDependency, InitializationValue, NullableInitializationValue, IArbitraryObject, IConstructorArgument, IMethodDeclaration, IMemberDeclaration, PropIndexer, IPositionable} from "./interface/ISimpleLanguageService";
+export {ISimpleLanguageService, IClassDeclaration, IModuleDependency, InitializationValue, NullableInitializationValue, IArbitraryObject, IConstructorArgument, IMethodDeclaration, IMemberDeclaration, PropIndexer, IPositionable, IPropertyCallExpression, ICallExpressionArgument, TypeArgument, AssignmentMap, ResolvedMethodMap} from "./interface/ISimpleLanguageService";
 export {ISimpleLanguageServiceConfig} from "./interface/ISimpleLanguageServiceConfig";
