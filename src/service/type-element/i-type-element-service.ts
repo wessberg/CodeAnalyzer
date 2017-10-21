9aad1cada57bc1d7ee98124a093af8683f83260d
wessberg <frederikwessberg@gmail.com>
2017-10-21 14:46:27 +0200

    Added a few methods to the TypeElement service

--- src/service/type-element/i-type-element-service.ts
+++ src/service/type-element/i-type-element-service.ts
@@ -1,2 +1,6 @@
+import {TypeElement} from "typescript";
+
 export interface ITypeElementService {
+	isOptional (typeElement: TypeElement): boolean;
+	getName (typeElement: TypeElement): string|undefined;
 }
--- src/service/type-element/type-element-service.ts
+++ src/service/type-element/type-element-service.ts
@@ -1,7 +1,28 @@
 import {ITypeElementService} from "./i-type-element-service";
+import {TypeElement} from "typescript";
+import {IPropertyNameService} from "../property-name/i-property-name-service";
 
 /**
  * A service for working with TypeElements
  */
 export class TypeElementService implements ITypeElementService {
+	constructor (private propertyNameService: IPropertyNameService) {}
+
+	/**
+	 * Returns true if the provided TypeElement has a questionToken
+	 * @param {TypeElement} typeElement
+	 * @returns {boolean}
+	 */
+	public isOptional (typeElement: TypeElement): boolean {
+		return typeElement.questionToken != null;
+	}
+
+	/**
+	 * Returns the name of the given TypeElement
+	 * @param {TypeElement} typeElement
+	 * @returns {string}
+	 */
+	public getName (typeElement: TypeElement): string|undefined {
+		return typeElement.name == null ? undefined : this.propertyNameService.getName(typeElement.name);
+	}
 }
